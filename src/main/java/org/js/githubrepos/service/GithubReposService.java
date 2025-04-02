a4d07b6fe79594140c6c033f101e27216d98b424
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-04-02 20:40:20 +0200

    1) Changed spring version to a higher version 3.3.5. 2) Added new application-dev.properties to use personal access token and not upload it on github. 3) GithubReposService in progress.

--- src/main/java/org/js/githubrepos/service/GithubReposService.java
+++ src/main/java/org/js/githubrepos/service/GithubReposService.java
@@ -1,7 +1,19 @@
 package org.js.githubrepos.service;
 
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GithubReposService {
     private static final String GITHUB_API_URL_BASE = "https://api.github.com";
+
+    private OkHttpClient okHttpClient;
+
+    @Autowired
+    public GithubReposService(OkHttpClient client) {
+        this.okHttpClient = client;
+    }
 
 
 }
