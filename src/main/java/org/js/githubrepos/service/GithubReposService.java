acd28e168773060f72cba6e092f5251300187d4a
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-04-03 23:57:41 +0200

    1) Changed some things in Swagger. 2) Added handling of the exceptions by the GlobalExceptionHandler. 3) Deleted some lines in README.md

--- src/main/java/org/js/githubrepos/service/GithubReposService.java
+++ src/main/java/org/js/githubrepos/service/GithubReposService.java
@@ -4,6 +4,8 @@ import lombok.extern.apachecommons.CommonsLog;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.apache.coyote.BadRequestException;
+import org.js.githubrepos.service.exceptions.GithubLoginNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,52 +18,39 @@ public class GithubReposService {
 
     private final OkHttpClient okHttpClient;
 
-    //@Value("${github.token:${GITHUB_TOKEN:}}")
-    private String githubToken;
-
     @Autowired
     public GithubReposService(OkHttpClient client) {
         this.okHttpClient = client;
     }
 
     public String executeListRepositoriesForUserEndpoint(String username) throws IOException {
-        log.info("GitHub Token: " + githubToken);
 
-        Request.Builder requestBuilder = new Request.Builder().url(GITHUB_API_URL_BASE + "/users/" + username +
-                        "/repos")
-                .header("Accept", "application/vnd.github+json")
-                .header("X-GitHub-Api-Version", "2022-11-28");
-
-        if (githubToken != null && !githubToken.trim()
-                .isEmpty()) {
-            requestBuilder.header("Authorization", "Bearer " + githubToken);
-        } else {
-            log.info("WARNING: GitHub token is not configured. Requests are limited to the rate limit of " +
-                    "the Github API.");
-        }
+        Request.Builder requestBuilder = new Request.Builder().url(GITHUB_API_URL_BASE + "/users/" + username + "/repos")
+            .header("Accept", "application/vnd.github+json")
+            .header("X-GitHub-Api-Version", "2022-11-28");
 
         Request request = requestBuilder.build();
 
         try (Response response = okHttpClient.newCall(request)
-                .execute()) {
+            .execute()) {
             if (!response.isSuccessful()) {
                 if (response.code() == 404) {
-                    throw new RuntimeException("User not found: " + username);
+                    throw new GithubLoginNotFoundException("Github login for given user not found: " + username);
                 }
 
                 if (response.code() == 403 && response.header("X-RateLimit-Remaining") != null && Integer.parseInt(response.header("X-RateLimit-Remaining")) == 0) {
-                    throw new RuntimeException("GitHub API rate limit exceeded. Reset at: " + response.header("X" +
-                            "-RateLimit-Reset"));
+                    throw new RuntimeException("GitHub API rate limit exceeded. Reset at: " + response.header("X" + "-RateLimit-Reset"));
                 }
+
                 throw new IOException("Unexpected response code: " + response);
             }
 
             if (response.body() == null) {
-                throw new IOException("Empty response body");
+                throw new BadRequestException("Provided input is empty.");
             }
 
             return response.body()
-                    .string();
+                .string();
         }
     }
 }
