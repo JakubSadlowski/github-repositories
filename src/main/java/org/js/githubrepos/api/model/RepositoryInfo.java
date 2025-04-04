ced76706ecabc9f6a080b7de8c05942ec227b07c
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-04-04 14:12:23 +0200

    1) Service is working as intended now need to add proper exceptions where it matters and make the code more clean.

--- src/main/java/org/js/githubrepos/api/model/RepositoryInfo.java
+++ src/main/java/org/js/githubrepos/api/model/RepositoryInfo.java
@@ -1,10 +1,12 @@
 package org.js.githubrepos.api.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class RepositoryInfo {
     private String repositoryName;
     private String ownerLogin;
